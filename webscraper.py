cb3e176bcbe90a4cb63214621d37f84bb07c6a89
Sam <samuelmatthews96@gmail.com>
2020-08-23 15:05:23 +0100

    Added getSoup, getAlternativeURL and basic functionality

--- webscraper.py
+++ webscraper.py
@@ -2,5 +2,30 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
-def scrape():
-    print('main function for this file')
+TEST_URL = 'https://en.wikipedia.org/wiki/Napoleonic_Wars'
+
+## main function that gets called from mail file
+def main():
+    print('main function')
+    soup = getSoup(TEST_URL)
+    heading = soup.find(id='firstHeading')
+    print(heading)
+
+
+def getAlternativeURL():
+    return 'https://en.wikipedia.org/wiki/Python_(programming_language)'
+
+def getSoup(url):
+    response = requests.get(url)
+
+    #if bad response, perform request on alternative URL
+    if response.status_code != 200:
+        url = getAlternativeURL()
+        response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    return soup
+
+main()
+
