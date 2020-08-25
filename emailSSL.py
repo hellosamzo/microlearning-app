bddc9fdb4de6b4caa0097dd2f4a8b5c78b66e2d8
Sam <samuelmatthews96@gmail.com>
2020-08-25 20:17:45 +0100

    Moved email sending to own method and added timed loop

--- emailSSL.py
+++ emailSSL.py
@@ -1,22 +1,31 @@
 import smtplib, ssl
 from webscraper import main as scraper
 from secrets import SENDER_EMAIL, RECEIVER_EMAIL, PASSWORD
+import time
 
 soupContent = scraper()
+#startTime = time.time()
 
-port = 465  # SSL
-smtp_server = "smtp.gmail.com"
-sender_email = SENDER_EMAIL
-receiver_email = RECEIVER_EMAIL 
-password = PASSWORD
-message = """\
-Subject: {0}
+def sendEmail():
+    port = 465  # SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = SENDER_EMAIL
+    receiver_email = RECEIVER_EMAIL 
+    password = PASSWORD
+    message = """\
+    Subject: {0}
 
-{1}
+    {1}
 
-This message was automated using Python.""".format(soupContent[0], soupContent[1])
+    This message was automated using Python.""".format(soupContent[0], soupContent[1])
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+while True:
+    #print('time')
+    sendEmail()
+    time.sleep(60 - time.time() % 60)
+
