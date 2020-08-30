14b53866a39bd78f195aa387d6e35822b7d4f56a
Sam <samuelmatthews96@gmail.com>
2020-08-30 13:58:36 +0100

    Added email.mime library to have proper subject etc. on email

--- emailSSL.py
+++ emailSSL.py
@@ -1,6 +1,8 @@
 import smtplib, ssl
 from webscraper import main as scraper
-from secrets import SENDER_EMAIL, RECEIVER_EMAIL, PASSWORD
+from secrets import SENDER_EMAIL, RECEIVER_EMAIL, PASSWORD#
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import time
 
 #startTime = time.time()
@@ -12,19 +14,28 @@ def sendEmail():
     sender_email = SENDER_EMAIL
     receiver_email = RECEIVER_EMAIL 
     password = PASSWORD
-    message = """\
-    Subject: {0}
+    
+    message = MIMEMultipart()
+    message["Subject"] = soupContent[0]
+    message["From"] = 'Microlearning App'
+    message["To"] = RECEIVER_EMAIL
+    body = """\
+    {0}
 
     {1}
 
     Read more about this at: {2}
 
     This message was automated using Python.""".format(soupContent[0], soupContent[1], soupContent[2])
+    
+    part1 = MIMEText(body, 'plain')
+
+    message.attach(part1)
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+        server.sendmail(sender_email, receiver_email, message.as_string())
 
 while True:
     #print('time')
