c992f418ba144cc69e76f3d7018c45cc7fd8545d
Sam <samuelmatthews96@gmail.com>
2020-11-12 19:06:24 +0000

    Added apiGET method to APIHandler.py

--- APIHandler.py
+++ APIHandler.py
@@ -1,8 +1,15 @@
 import requests
 
-def apiPOST():
-
-    url = 'http://localhost:3000/'
+def apiPOST(url):
+    if url is None:
+        url = 'http://localhost:3000/'
     x = requests.post(url)
+    #print(x.text)
+    return x.text
+
+def apiGET(url):
+    if url is None:
+        url = 'http://localhost:3000/'
+    x = requests.get(url)
     #print(x.text)
     return x.text
