039b200677da55c79da0c0868da1097c9e71c859
Sam <samuelmatthews96@gmail.com>
2020-11-12 19:10:00 +0000

    Added initial apiExtractor method to APIHandler.py

--- APIHandler.py
+++ APIHandler.py
@@ -12,4 +12,9 @@ def apiGET(url):
         url = 'http://localhost:3000/'
     x = requests.get(url)
     #print(x.text)
+    extractedURL = apiExtractor(x)
     return x.text
+
+def apiExtractor(data):
+    urlTest = data['results'][0]['url']['genre']
+    return urlTest
